1e47a4caafa18ebab589b3e64bd1c776997c1fe0
Дмитрий Христенко <medmikhr@yandex.ru>
2024-06-20 14:59:04 +0300

    А можно написать выражение, создающее объекты новостей внутри bulk_create():

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -9,8 +9,7 @@ class TestContent(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        all_news = [
+        News.objects.bulk_create(
             News(title=f'Новость {index}', text='Просто текст.')
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-        ]
-        News.objects.bulk_create(all_news)
+        )
