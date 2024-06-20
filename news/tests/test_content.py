52e73775fb152c71968af7dcbb763f4f16db9a03
Дмитрий Христенко <medmikhr@yandex.ru>
2024-06-20 15:05:14 +0300

    Остаётся получить длину списка с объектами новостей и сравнить её с константой из настроек

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,15 +1,31 @@
 # news/tests/test_content.py
 from django.conf import settings
 from django.test import TestCase
+# Импортируем функцию reverse(), она понадобится для получения адреса страницы.
+from django.urls import reverse
 
 from news.models import News
 
 
-class TestContent(TestCase):
+class TestHomePage(TestCase):
+    # Вынесем ссылку на домашнюю страницу в атрибуты класса.
+    HOME_URL = reverse('news:home')
 
     @classmethod
     def setUpTestData(cls):
-        News.objects.bulk_create(
+        all_news = [
             News(title=f'Новость {index}', text='Просто текст.')
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-        )
+        ]
+        News.objects.bulk_create(all_news)
+
+    def test_news_count(self):
+        # Загружаем главную страницу.
+        response = self.client.get(self.HOME_URL)
+        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        # Определяем количество записей в списке.
+        news_count = object_list.count()
+        # Проверяем, что на странице именно 10 новостей.
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
