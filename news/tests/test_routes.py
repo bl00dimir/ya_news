2673563f3de7acdb2d2e7ed7cabaad0a1e0d9549
Дмитрий Христенко <medmikhr@yandex.ru>
2024-06-20 14:21:28 +0300

    Теперь проверим, что страницы редактирования и удаления комментария доступны автору комментария, но недоступны другому пользователю. Для программного создания пользователей принято использовать метод Django ORM create_user(), который правильным образом устанавливает пароль для пользователя. Но в тестах мы пароли не используем — и можем обойтись обычным create().

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,10 +1,16 @@
 # news/tests/test_routes.py
 from http import HTTPStatus
 
+# Импортируем функцию для определения модели пользователя.
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from news.models import News
+# Импортируем класс комментария.
+from news.models import Comment, News
+
+# Получаем модель пользователя.
+User = get_user_model()
 
 
 class TestRoutes(TestCase):
@@ -12,6 +18,15 @@ class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Создаём двух пользователей с разными именами:
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+        # От имени одного пользователя создаём комментарий к новости:
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text='Текст комментария'
+        )
 
     def test_pages_availability(self):
         urls = (
